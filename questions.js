addfe9a3cbb1d259d7723a9b4ba2dabd68efac05
Nishigandha-Kasar <143502853+Nishigandha-Kasar@users.noreply.github.com>
2024-05-23 20:55:49 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,107 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question:"Inside which HTML element do we put the Javascript?",
+     answer: "<javascript>",
+     options: [
+        "<js>",,
+        "<script>",
+        "<javascript>",
+        "<scripting>"
+     ]
+   },
+   {
+    numb: 7,
+    question:"How to write an IF statement in JavaScript?",
+     answer:"",
+     options: [
+          
+    ]  
+     
+   },
+];
+
+
+
+// {
+// question:"Where is the correct place to insert a JavaScript?",
+// answers:[
+//     { text:"The <body> section" , correct: false},
+//     { text:"Both the <head> section and the <body> section are correct" , correct:true},
+//     { text:"The <script> section" ,correct: false},
+//     { text:"The <head> section" ,correct: false},
+// ]  
+// },
+// {
+// question:"How to write an IF statement in JavaScript?",
+// answers:[
+//     { text:"if i=5 then", correct: false},
+//     { text:"if(i==5)", correct: true},
+//     { text:"if i==5 then", correct: false},
+//     { text:"if i=5", correct: false},
+// ]  
+// },
+// {
+// question:"How does a WHILE loop start?",
+// answers:[
+//     { text:"while(i <= 10; i++", correct: false},
+//     { text:"while i=1 to 10", correct: false},
+//     { text:"while (i <=10)", correct: true},
+    
+// ]  
+// },
